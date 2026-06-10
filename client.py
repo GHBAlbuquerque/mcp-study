7281834ee0a53c9b329bad80cc20a25688657002
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2026-06-10 17:59:15 -0300

    feature: implements a wikipedia search tool and a client with an agent to summarize the response

--- client.py
+++ client.py
@@ -1,17 +1,45 @@
 import asyncio
 
 from fastmcp import Client
+from openai import OpenAI
+
+import dotenv
+import os
+
+dotenv.load_dotenv()
 
 server_path = "http://localhost:8000/sse"
 client = Client(server_path)
 
-async def call_server(client: Client, username: str, user_id: int):
+async def call_server(client: Client, search: str,):
+    api_key=os.getenv("OPENAI_API_KEY")
+    
+    
     async with client:
-        arguments = {"username": username, "user_id": user_id}
-        result = await client.call_tool("hello", arguments)
+        arguments = {"search": search}
+        result = await client.call_tool("search_wikipedia", arguments)
+        print(f'\n==================================================================\n')
         print(result)
+        system_message=f"""
+            You are a bot that runs searches on wikipedia.
+            User has searched: {search}
+            For this search, you received the following answer: {result}
+            Format a friendly response to the user based on the search result.
+        """
+        
+        client_openai=OpenAI(api_key=api_key)
+        
+        response=client_openai.responses.create(
+            model="gpt-5-nano",
+            instructions=system_message,
+            input="Please summarize this search result for me.",
+        )
+        print(f'\n==================================================================\n')
+        print(response.output_text)
+        
+        
 
 if __name__== "__main__":
-      asyncio.run(call_server(client, "gigibene", 123456789))
+      asyncio.run(call_server(client, "Broken Earth Series"))
       
       
