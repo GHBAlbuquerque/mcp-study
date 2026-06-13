907a164294fb3ec719256964ec2155d271ab28f0
GHBAlbuquerque <ghb.albuquerque@gmail.com>
2026-06-13 16:36:00 -0300

    feat: created stdio server for claude desktop connection

--- server_claude_wikipedia.py
+++ server_claude_wikipedia.py
@@ -5,11 +5,11 @@ from wikipedia import wikipedia
 server_mcp = FastMCP("mcp-server")
 
 @server_mcp.tool() # mcp servers run async to avoid blocking, so all tools should be async
-async def search_wikipedia(search: str) -> str: #adding types helps IA undestand what the function does
+async def search_wikipedia_locally(search: str) -> str: #adding types helps IA undestand what the function does
 	return wikipedia.summary(search)
 
 
 if __name__ == "__main__":
-	server_mcp.run(transport="sse") #server side execution
+	server_mcp.run(transport="stdio") #standard input output
  
  
